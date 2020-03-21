5eee08d7d2fce3d429d09cba1dfc54647af75c92
dev-mg <mick.groff@devmg.dev>
2020-03-21 14:37:03 -0600

    -svelte-is-amazing-

--- src/App.svelte
+++ src/App.svelte
@@ -1,10 +1,12 @@
 <script>
 	export let name;
+	import Com from './com.svelte';
 </script>
 
 <main>
 	<h1>Hello {name}!</h1>
 	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	<Com/>
 </main>
 
 <style>
