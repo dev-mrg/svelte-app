f933ebfb76f8a41ffac9d0bd7f1b96f55f430dc9
dev-mg <mick.groff@devmg.dev>
2020-03-21 16:55:54 -0600

    -amazing-

--- src/App.svelte
+++ src/App.svelte
@@ -21,12 +21,7 @@ import Stuff from './main.svelte';
 		margin: 0 auto;
 	}
 
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
+
 
 	@media (min-width: 640px) {
 		main {
